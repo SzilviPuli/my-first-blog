152dce70619c561941a23e290106f6abf2a91d5b
szilvi <szilvia@pulilab.com>
2015-05-16 15:31:52 +0200

    My Django Girls app, first commit

---
+++ firstdjango.py
@@ -0,0 +1,10 @@
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'Szilvi']
+def hi(name):
+    print('Hi ' + name + '!')
+
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'Szilvi']
+for name in girls:
+    hi(name)
+    print('Next girl')
+for i in range(1, 6):
+    print(i)
