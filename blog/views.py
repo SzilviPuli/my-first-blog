a8b5f7fb448afe99eb2f3059418c3a3a48078db9
szilvi <szilvia@pulilab.com>
2015-05-17 22:41:20 +0200

    Changes in adding new post and saving

--- blog/views.py
+++ blog/views.py
@@ -11,3 +11,16 @@ from django.shortcuts import render, get_object_or_404
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+from .forms import PostForm
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
